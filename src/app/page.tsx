dc00c8e2d5249c2e57019f9d970bd4b69c0b16a7
Ajinkya Kale <ajinkode@gmail.com>
2025-08-06 21:25:32 +0530

    feat: Create skeleton structural foundation for TipTap editor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import {Button} from "@/components/ui/button";
+import React from "react";
+import Link from "next/link";
 
 const Home = () => {
-    return (
-        <div className="flex justify-center items-center min-h-screen ">
-            <Button>
-                Click Me
-            </Button>
-        </div>
-    )
-}
-export default Home
+  return (
+    <div className="flex justify-center items-center min-h-screen ">
+      Click{" "}
+      <Link href="/documents/123">
+        {" "}
+        <span className="text-blue-500 underline">&nbsp;here&nbsp;</span>
+      </Link>{" "}
+      to go to the document id
+    </div>
+  );
+};
+
+export default Home;
