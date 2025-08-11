b8cfce7472fe83c1c0b00def3684f5deebfc49fa
efficientgoose <ajinkode@gmail.com>
2025-08-11 11:22:19 +0530

    feat: Add basic editor tools in Toolbar

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -11,6 +11,7 @@ import { TableHeader } from "@tiptap/extension-table-header";
 import { Image } from "@tiptap/extension-image";
 import { ImageResize } from "tiptap-extension-resize-image";
 import { useEditorStore } from "@/store/use-editor-store";
+import Underline from "@tiptap/extension-underline";
 
 const Editor = () => {
   const { setEditor } = useEditorStore();
@@ -50,6 +51,7 @@ const Editor = () => {
     immediatelyRender: false,
     extensions: [
       StarterKit,
+      Underline,
       TaskList,
       TaskItem.configure({ nested: true }),
       Table,
